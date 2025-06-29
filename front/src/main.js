67647d1d2667a28a806b4ecd3e0eddbf1efd2e41
gggggggggg564 <2034543231@qq.com>
2025-06-29 13:49:40 +0800

    Add files via upload

--- front/src/main.js
+++ front/src/main.js
@@ -2,9 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
 import "@fortawesome/fontawesome-free/css/all.css";
 
 const app = createApp(App);
 app.use(router);
 app.use(store);
+store.dispatch('checkAuth')
 app.mount("#app");
